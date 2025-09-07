37f54cc54ba206feadd061859f43e355a789ab9c
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2025-09-07 13:06:21 +0300

    Added API

--- src/Classes/RandomQuotesApp.js
+++ src/Classes/RandomQuotesApp.js
@@ -1,8 +1,12 @@
+import Quote from './Quote.js';
 import RandomQuote from './RandomQuote.js';
 
 class RandomQuotesApp {
   constructor() {
     this.randomQuoteBtn = document.getElementById('random-quote-btn');
+    this.randomQuoteAPIBtn = document.getElementById(
+      'random-quote-api-btn'
+    );
     this.quoteTextElement = document.getElementById('quote-text');
     this.quoteAuthorElement = document.getElementById('quote-author');
     this.currentQuote = null;
@@ -10,21 +14,34 @@ class RandomQuotesApp {
     this.init();
   }
   displayCurrentQuote() {
-    const { author } = this.currentQuote;
     this.quoteTextElement.textContent = this.currentQuote.formatText();
     this.quoteAuthorElement.textContent =
       this.currentQuote.formatAuthor();
   }
 
+  changeCurrentQuote(newQuote) {
+    if (newQuote instanceof Quote) {
+      this.currentQuote = newQuote;
+      this.displayCurrentQuote();
+    }
+  }
+
   getRandomQuote() {
-    const randomQuote = RandomQuote.getRandomQuote();
-    this.currentQuote = randomQuote;
-    this.displayCurrentQuote();
+    this.changeCurrentQuote(RandomQuote.getRandomQuote());
+  }
+
+  getRandomQuoteViaAPI() {
+    RandomQuote.getRandomQuoteViaAPI().then((quote) =>
+      this.changeCurrentQuote(quote)
+    );
   }
 
   init() {
     this.randomQuoteBtn.addEventListener('click', () =>
       this.getRandomQuote()
+    );
+    this.randomQuoteAPIBtn.addEventListener('click', () =>
+      this.getRandomQuoteViaAPI()
     );
   }
 }
